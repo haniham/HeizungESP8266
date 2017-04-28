655c789e04f37eff88f2f48f8cf7544f5513a628
haniham <markushahnenkamm@gmail.com>
2017-04-28 17:34:24 +0200

    Implemented an Array of Parameterelements and removed deprecated switch-case-returns

--- src/Parameterliste.cpp
+++ src/Parameterliste.cpp
@@ -1,25 +1,15 @@
 #include "Parameterliste.h"
 
-Parametertyp getParametertyp(byte ParameterNr){
-  switch (ParameterNr) {
-    case 4: return Analog2b;
-    case 5: return Stat0F;
-    case 23: return Analog2b_Sensor;
-    case 24: return Analog2b_Sensor;
-    case 57: return Analog2b;
-    case 68: return Stat01;
-  }
-}
-
-String getParameterBeschreibung(byte ParameterNr)
+Parameterelement* getParameterelement(byte parameterNr)
 {
-  switch (ParameterNr){
-    case 4: return "Sollwert_Speicher";
-    case 5: return "Flammsignal";
-    case 23: return "Speichertemperatur";
-    case 24: return "Vorlauftemperatur";
-    case 57: return "Sollwert_Vorlauf";
-    case 68: return "Interne_Heizungspumpe";
+  //Performing a search over all elements
+  for(byte i=0;i<parameterelementsSize;i++)
+  {
+    if(parameterelements[i].Nr==parameterNr)
+      //returns element poointer when found
+      return &parameterelements[i];
   }
-  return "";
+
+  //Elsewise returns a null pointer
+  return NULL;
 }
--- src/Parameterliste.h
+++ src/Parameterliste.h
@@ -5,14 +5,30 @@
 #include <Stream.h>
 #include <string>
 
+//Enum for holding the Type of parameter
 enum Parametertyp {Keiner, Stat01, Stat0F, Analog1b, Analog2b, Analog2b_Sensor };
 
-class ParameterBeschreibung {
-    Parametertyp parametertyp;
-    std::string beschreibung;
+//Structure holding the Parameter Number, its Name and the PArameter Type
+struct Parameterelement{
+  byte Nr;
+  String Name;
+  Parametertyp parametertyp;
 };
 
-Parametertyp getParametertyp(byte ParameterNr);
-String getParameterBeschreibung(byte ParameterNr);
+//Array holding all parameteremlements
+Parameterelement parameterelements[] = {
+  {4,"Sollwert_Speicher",Analog2b},
+  {5,"Flammsignal",Stat0F},
+  {23,"Speichertemperatur",Analog2b_Sensor},
+  {24,"Vorlauftemperatur",Analog2b_Sensor},
+  {57,"Sollwert_Vorlauf",Analog2b},
+  {68,"Interne_Heizungspumpe",Stat01}
+};
+byte parameterelementsSize = sizeof (parameterelements) / sizeof *(parameterelements);
 
+/*
+/ Searches the Parameterelements for the corresponding element
+/ Returns NULL if not found
+*/
+Parameterelement* getParameterelement(byte parameterNr);
 #endif
