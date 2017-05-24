9cdaa5e7d540ac9da480f9c37fd4967ac1332dad
haniham <markushahnenkamm@gmail.com>
2017-05-25 00:39:33 +0200

    Added Parameter definitions

--- src/Parameterliste.h
+++ src/Parameterliste.h
@@ -6,9 +6,9 @@
 #include <string>
 
 //Enum for holding the Type of parameter
-enum Parametertyp {Keiner=0, Stat01, Stat0F, Analog1b, Analog2b, Analog2b_Sensor };
+enum Parametertyp {Keiner=0, Stat01, Stat0F, Analog1b, Analog2b, Analog2b_Sensor,Fehlerspeicher };
 // Hplding the Name for the Enums
-const char* ParametertypName[] = {"Keiner", "Stat01", "Stat0F", "Analog1b", "Analog2b", "Analog2b_Sensor"};
+const char* ParametertypName[] = {"Keiner", "Stat01", "Stat0F", "Analog1b", "Analog2b", "Analog2b_Sensor", "Fehlerspeicher"};
 //Returning the Name of the Enum
 const char* getParametertypName(Parametertyp parametertyp);
 
@@ -27,12 +27,36 @@ struct Parameterelement{
 
 //Array holding all parameteremlements
 const Parameterelement parameterelements[] = {
+  {2,"Sollwert_Brauchwasser",Analog2b},
   {4,"Sollwert_Speicher",Analog2b},
   {5,"Flammsignal",Stat0F},
+  {6,"Fehlerspeicher",Fehlerspeicher},
+  {8,"SommerWinter",Stat0F},
+  {14,"Stat_Raumthermostat",Stat01},
+  {22,"Brauchwassersensor",Analog2b_Sensor},
   {23,"Speichertemperatur",Analog2b_Sensor},
   {24,"Vorlauftemperatur",Analog2b_Sensor},
+  {31,"Anzahl_GFA_Stoerungen",Analog1b},
+  {32,"Anzahl_STB_Abschaltungen",Analog1b},
+  {36,"Drezahlsollwert",Analog2b},
+  {37,"Sollwert_Vorlauf_Ext_Regler",Analog2b},
+  {56,"Verbleibende_Brennersperrzeit",Analog1b},
   {57,"Sollwert_Vorlauf",Analog2b},
-  {68,"Interne_Heizungspumpe",Stat01}
+  {63,"Externe_Heizungspumpe",Stat01},
+  {66,"Stellung_VUV",Analog1b},
+  {68,"Interne_Heizungspumpe",Stat01},
+  {88,"Anforderung_Warmwasser_C1C2",Stat01},
+  {99,"Wasserpumpennachlaufzeit_nach_Speicherladung",Analog1b},
+  {100,"Wasserpumpennachlaufzeit_nach_Heizbetrieb",Analog1b},
+  {101,"Korrekturwert_Aussentemperatur",Analog1b},
+  {102,"Speicherladezeit",Analog1b},
+  {106,"Aussentemperatur",Analog2b_Sensor},
+  {107,"Ext_VorRuecklauftemp",Analog2b_Sensor},
+  {108,"Heizungszeillast",Analog1b},
+  {111,"Zaehler_3_Zuendversuche",Analog1b},
+  {113,"Speicherfreigab_Schaltuhr",Stat01},
+  {131,"Drezahlistwert",Analog2b}
+
 };
 const byte parameterelementsSize = sizeof (parameterelements) / sizeof *(parameterelements);
 
